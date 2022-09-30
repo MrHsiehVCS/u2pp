5bb8feb5ee918b26c2c3a808b415bcf5454240ae
MrHsiehVCS <erich@villagechristian.org>
2022-09-30 15:34:48 -0700

    Clean up files & build settings

---
+++ src/main/java/u2pp/Main.java
@@ -0,0 +1,13 @@
+package u2pp; // new files in this project need to include this line
+
+public class Main 
+{
+    public static void main(String[] args) {
+        
+        // Read the README for the assignment. 
+        
+        // Use this space to test your code, or actually run your project
+        System.out.println("Hi! This is an empty project.\nYour code will start running from Main.java.\nIt may be daunting to start a project from scratch, but try to have fun with it! :)");
+
+    }
+}
